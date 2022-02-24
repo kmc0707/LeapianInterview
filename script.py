e474428a3b09a591a2e86a6aa6d6c4483f5c4226
kmc0707 <kmc0707@hotmail.com>
2022-02-24 18:24:14 +0000

    y axis mmm

--- script.py
+++ script.py
@@ -1,3 +1,6 @@
+from statistics import median
+
+from sympy import total_degree
 from dataset import data
 from word_class import Word
 
@@ -28,8 +31,38 @@ def words_sorted_by_word_height(DicOfWords):
             Dic_sorted[word.wordHeight] = [word]
     return Dic_sorted
 
+def words_sorted_by_y_coordinate(DicOfWords,key):
+    array_of_words=DicOfWords[key]
+    Dic_sorted = {}
+    for word in array_of_words:
+        if word.wordY in Dic_sorted:
+            Dic_sorted[word.wordY].append(word)
+        else:
+            Dic_sorted[word.wordY] = [word]
+    return Dic_sorted
 
 
+def mmm_y_axis(DicOfWords):
+    highest_num = -1
+    mode = -1
+    total = 0
+    mean = -1
+    highest = -1
+    lowest = -1
+    median = -1
+    for key in DicOfWords.keys():
+        total += len(DicOfWords[key])
+        mean += len(DicOfWords[key]) * key
+        if len(DicOfWords[key]) > highest_num:
+            highest_num = len(DicOfWords[key])
+            mode = key
+        if key > highest:
+            highest = key
+        if key < lowest:
+            lowest = key
+    median = (highest - lowest)/2
+    mean = mean/total
+    print("meadian: " , median, " mode: ", mode, " mean: ", mean)
 
 if __name__ == "__main__":
     
@@ -37,4 +70,6 @@ if __name__ == "__main__":
     Dic_sorted = words_sorted_by_word_height(DicOfWords)
     for key in Dic_sorted.keys():
         print(key,  " " , len(Dic_sorted[key]))
+    Dic_sorted_y = words_sorted_by_y_coordinate(Dic_sorted,8)
+    mmm_y_axis(Dic_sorted_y)
 
